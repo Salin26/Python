4451aa6f4a8d6a61446b92c194d9faadec5349e9
Mushfiqur Rahman Salin <amisalin2@gmail.com>
2026-01-21 09:43:13 +0600

    Create list.py

---
+++ list.py
@@ -0,0 +1,16 @@
+list=[2,4,1]
+
+list.append(5) 
+print("after append: ",list)
+
+list.sort()
+print("after sort: ",list)
+
+list.reverse()
+print("after reverse: ",list)
+
+list.insert(2,8)
+print("after insert: ",list)
+
+list.pop(2)
+print("after pop: ",list)
